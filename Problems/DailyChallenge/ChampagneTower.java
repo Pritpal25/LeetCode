9aad21289a5ccb687a8b73e46f533c082bd31bb9
Pritpal Singh Banga <pbanga@amazon.com>
2020-10-26 18:19:16 -0700

    LC Daily Challenge : Champagne Tower

---
+++ Problems/DailyChallenge/ChampagneTower.java
@@ -0,0 +1,21 @@
+package DailyChallenge;
+
+public class ChampagneTower
+{
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] tower = new double[100][100];
+
+        tower[0][0] = poured;
+        for (int r = 0; r < query_row; r++) {
+            for(int c = 0; c <= r; c++) {
+                double spill_over = (tower[r][c] - 1.0)/2.0;  // this is spillover on each side
+                if (spill_over > 0.0) {
+                    tower[r+1][c] += spill_over;
+                    tower[r+1][c+1] += spill_over;
+                }
+            }
+        }
+
+        return Math.min(1.0, tower[query_row][query_glass]);
+    }
+}
