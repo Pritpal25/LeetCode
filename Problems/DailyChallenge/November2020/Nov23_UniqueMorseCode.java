6b005a7e4f63d64ca34b593023aaff80012dfb46
Pritpal Singh Banga <pbanga@amazon.com>
2020-11-22 08:29:58 -0800

    Nov 23 : Unique Morse Code

---
+++ Problems/DailyChallenge/November2020/Nov23_UniqueMorseCode.java
@@ -0,0 +1,27 @@
+package DailyChallenge.November2020;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Nov23_UniqueMorseCode
+{
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] dict = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> result = new HashSet<>();
+
+        for(String word : words) {
+            StringBuilder str = new StringBuilder();
+
+            char[] wordArr = word.toCharArray();
+
+            for(char c : wordArr) {
+                str.append(dict[c-'a']);
+            }
+
+            result.add(str.toString());
+        }
+
+        return result.size();
+    }
+}
